2d8ee0f90d028284d95e4e01b04444063ece571f
icefairy <860668820@qq.com@7bebfb45-794e-4009-aaf3-670e3a229adc>
2016-04-12 09:55:14 +0000

    add roundimageview

---
+++ b4alibs/rivdemo/Objects/src/b4a/example/designerscripts/LS_1.java
@@ -0,0 +1,17 @@
+package b4a.example.designerscripts;
+import anywheresoftware.b4a.objects.TextViewWrapper;
+import anywheresoftware.b4a.objects.ImageViewWrapper;
+import anywheresoftware.b4a.BA;
+
+
+public class LS_1{
+
+public static void LS_general(java.util.LinkedHashMap<String, anywheresoftware.b4a.keywords.LayoutBuilder.ViewWrapperAndAnchor> views, int width, int height, float scale) {
+anywheresoftware.b4a.keywords.LayoutBuilder.setScaleRate(0.3);
+//BA.debugLineNum = 2;BA.debugLine="AutoScaleAll"[1/General script]
+anywheresoftware.b4a.keywords.LayoutBuilder.scaleAll(views);
+//BA.debugLineNum = 3;BA.debugLine="RIV1.Height=RIV1.Width"[1/General script]
+views.get("riv1").vw.setHeight((int)((views.get("riv1").vw.getWidth())));
+
+}
+}
