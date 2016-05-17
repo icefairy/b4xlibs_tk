ce8eb27a523087fd3240c041a31e7f96f38bfe33
icefairy <860668820@qq.com>
2016-05-17 11:55:36 +0800

    getui 源码转移

---
+++ b4alibs/galleryfinaldemo/b4aGalleryFinal/src/cn/finalteam/galleryfinal/PhotoViewWrapper.java
@@ -0,0 +1,78 @@
+package cn.finalteam.galleryfinal;
+
+import android.graphics.Bitmap;
+import android.support.v4.media.MediaBrowserCompat.MediaItem.Flags;
+import android.widget.ImageView.ScaleType;
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.BA.ShortName;
+import anywheresoftware.b4a.keywords.Common.DesignerCustomView;
+import anywheresoftware.b4a.objects.LabelWrapper;
+import anywheresoftware.b4a.objects.PanelWrapper;
+import anywheresoftware.b4a.objects.ViewWrapper;
+import anywheresoftware.b4a.objects.collections.Map;
+import anywheresoftware.b4a.objects.drawable.CanvasWrapper.BitmapWrapper;
+import cn.finalteam.galleryfinal.widget.zoonview.PhotoView;
+
+@ShortName("PhotoView")
+public class PhotoViewWrapper extends ViewWrapper<PhotoView> implements DesignerCustomView {
+	private PhotoView pv;
+	@Override
+	public void DesignerCreateView(PanelWrapper base, LabelWrapper lw, Map arg2) {
+		// TODO Auto-generated method stub
+		base.AddView(pv, 0, 0, base.getWidth(), base.getHeight());
+	}
+
+	@Override
+	public void _initialize(BA ba, Object cb, String en) {
+		// TODO Auto-generated method stub
+		pv=new PhotoView(ba.context);
+		setObject(pv);
+	}
+	public void setImageBitmap(Bitmap bm) {
+		getObject().setImageBitmap(bm);
+	}
+	public void setMinimumScale(float minimumScale){
+		getObject().setMinimumScale(minimumScale);
+	}
+	public void setMaximumScale(float maximumScale){
+		getObject().setMaximumScale(maximumScale);
+	}
+	public float getMinimumScale(){
+		return getObject().getMinimumScale();
+	}
+	public float getMaximumScale(){
+		return getObject().getMaximumScale();
+	}
+	public float getMediumScale(){
+		return getObject().getMediumScale();
+	}
+	public void setMediumScale(float mediumScale){
+		getObject().setMediumScale(mediumScale);
+	}
+	public boolean getcanZoom() {
+		return getObject().canZoom();
+	}
+	public void setScale(float scale, boolean animate) {
+		getObject().setScale(scale, animate);;
+	}
+	public float getScale(float scale, boolean animate) {
+		return getObject().getScale();
+	}
+	public void setScaleType(ScaleType scaleType) {
+		getObject().setScaleType(scaleType);
+	}
+	public ScaleType getScaleType() {
+		return getObject().getScaleType();
+	}
+	public void setRotationBy(float rotationDegree) {
+		getObject().setRotationBy(rotationDegree);
+	}
+	public void setRotationTo(float rotationDegree) {
+		getObject().setRotationTo(rotationDegree);
+	}
+	public BitmapWrapper getVisibleRectangleBitmap() {
+		BitmapWrapper bw=new BitmapWrapper();
+		bw.setObject(getObject().getVisibleRectangleBitmap());
+		return bw;
+	}
+}
