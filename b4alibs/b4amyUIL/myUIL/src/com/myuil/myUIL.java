d9819e319a3ad6ef1d4c1d4f9e7436de1a1cdb0b
icefairy <860668820@qq.com>
2016-07-08 17:04:15 +0800

    update uil

--- b4alibs/b4amyUIL/myUIL/src/com/myuil/myUIL.java
+++ b4alibs/b4amyUIL/myUIL/src/com/myuil/myUIL.java
@@ -1,30 +1,38 @@
 package com.myuil;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.display.CircleBitmapDisplayer;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
+import android.graphics.Bitmap;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import anywheresoftware.b4a.BA;
 import anywheresoftware.b4a.BA.Author;
 import anywheresoftware.b4a.BA.DependsOn;
+import anywheresoftware.b4a.BA.Events;
 import anywheresoftware.b4a.BA.Permissions;
 import anywheresoftware.b4a.BA.ShortName;
 import anywheresoftware.b4a.BA.Version;
 import anywheresoftware.b4a.objects.ImageViewWrapper;
+import anywheresoftware.b4a.objects.drawable.CanvasWrapper.BitmapWrapper;
 
 @ShortName("myUIL")
-@Version(1.10f)
+@Version(1.20f)
 @Author("Icefairy333")
 @DependsOn(values={"universal-image-loader-1.9.5"})
 @Permissions(values={"android.permission.INTERNET","android.permission.ACCESS_NETWORK_STATE","android.permission.WRITE_EXTERNAL_STORAGE","android.permission.READ_EXTERNAL_STORAGE"})
+@Events(values={"onLoadingFailed(url As String,Reason As String)","onLoadingComplete(url as String,bmp as Bitmap)"})
 public class myUIL {
 	private BA mba;
 	private String EN;
 	public boolean memcache=false;
 	public boolean diskcache=true;
 	public boolean resetViewBeforeLoading=false;
+	public boolean ViewBGTransparent=true;
 	public int delayBeforeLoading=0;
 	public int nLoadResId=0;
 	public int nFailResId=0;
@@ -35,10 +43,40 @@ public class myUIL {
 	public ScaleType MATRIX=ScaleType.MATRIX;
 	public ScaleType FIT_START=ScaleType.FIT_START;
 	public ScaleType FIT_END=ScaleType.FIT_END;
+	private ImageLoadingListener ill;
 	public void init(BA ba,String eventname) {
 		EN=eventname.toLowerCase();
 		mba=ba;
 		ToolImage.initImageLoader(ba.context);
+		ill=new ImageLoadingListener() {
+			
+			@Override
+			public void onLoadingStarted(String arg0, View arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLoadingFailed(String arg0, View arg1, FailReason arg2) {
+				// TODO Auto-generated method stub
+				mba.raiseEventFromDifferentThread(this, this, 0, EN+"_onLoadingFailed".toLowerCase(), false, new Object[]{arg0,arg2.toString()});
+			}
+			
+			@Override
+			public void onLoadingComplete(String arg0, View arg1, Bitmap arg2) {
+				// TODO Auto-generated method stub
+				BitmapWrapper bw=new BitmapWrapper();
+				bw.setObject(arg2);
+				mba.raiseEventFromDifferentThread(this, this, 0, EN+"_onLoadingComplete".toLowerCase(), false, new Object[]{arg0,bw});
+			}
+			
+			@Override
+			public void onLoadingCancelled(String arg0, View arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		ToolImage.getImageLoader().setDefaultLoadingListener(ill);
 	}
 	/**
 	 * Acceptable URIs examples
@@ -65,7 +103,9 @@ public class myUIL {
 		if(nFailResId>0){
 			dio.showImageOnFail(nFailResId);
 		}
-		ImageViewWrapper ivw;
+		if (ViewBGTransparent) {
+			iv.setBackgroundColor(0);
+		}
 		ToolImage.getImageLoader().displayImage(uri, iv,dio.build());
 	}
 	public void ShowCircleImage(ImageView iv,String uri,int strokeColor,int strokeWidth) {
@@ -82,6 +122,9 @@ public class myUIL {
 		}
 		if(nFailResId>0){
 			dio.showImageOnFail(nFailResId);
+		}
+		if (ViewBGTransparent) {
+			iv.setBackgroundColor(0);
 		}
 		dio.displayer(new CircleBitmapDisplayer(strokeColor,strokeWidth));
 		ToolImage.getImageLoader().displayImage(uri, iv,dio.build());
@@ -101,8 +144,14 @@ public class myUIL {
 		if(nFailResId>0){
 			dio.showImageOnFail(nFailResId);
 		}
+		if (ViewBGTransparent) {
+			iv.setBackgroundColor(0);
+		}
 		dio.displayer(new RoundedBitmapDisplayer(cornerRadiusPixels,marginPixels));
 		ToolImage.getImageLoader().displayImage(uri, iv,dio.build());
+	}
+	public void DownloadPic(String url) {
+		ToolImage.getImageLoader().loadImage(url,ill);
 	}
 	public void ClearCache() {
 		ToolImage.clearCache();
