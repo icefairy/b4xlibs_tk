a900c1fbcfdaecdb26e390c57d5ced0b8c31d015
icefairy <860668820@qq.com@7bebfb45-794e-4009-aaf3-670e3a229adc>
2016-10-11 03:20:08 +0000

    add fastblurutil lib

---
+++ b4alibs/b4aFastBlurUtil/demo/Objects/src/b4a/example/designerscripts/LS_1.java
@@ -0,0 +1,17 @@
+package b4a.example.designerscripts;
+import anywheresoftware.b4a.objects.TextViewWrapper;
+import anywheresoftware.b4a.objects.ImageViewWrapper;
+import anywheresoftware.b4a.BA;
+
+
+public class LS_1{
+
+public static void LS_general(java.util.LinkedHashMap<String, anywheresoftware.b4a.keywords.LayoutBuilder.ViewWrapperAndAnchor> views, int width, int height, float scale) {
+anywheresoftware.b4a.keywords.LayoutBuilder.setScaleRate(0.3);
+//BA.debugLineNum = 2;BA.debugLine="AutoScaleAll"[1/General script]
+anywheresoftware.b4a.keywords.LayoutBuilder.scaleAll(views);
+//BA.debugLineNum = 3;BA.debugLine="ivsmall.HorizontalCenter=50%x"[1/General script]
+views.get("ivsmall").vw.setLeft((int)((50d / 100 * width) - (views.get("ivsmall").vw.getWidth() / 2)));
+
+}
+}
