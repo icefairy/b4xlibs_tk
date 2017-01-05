7160bb41fc5f96f5425cd4305c86943eeb6f5734
icefairy <860668820@qq.com>
2017-01-05 12:29:35 +0800

    更新bugly（增加自动更新模块）

--- b4alibs/bugly/b4aBugly/src/com/bugly/buglywrapper.java
+++ b4alibs/bugly/b4aBugly/src/com/bugly/buglywrapper.java
@@ -1,5 +1,7 @@
 package com.bugly;
 
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import anywheresoftware.b4a.BA;
@@ -11,18 +13,22 @@ import anywheresoftware.b4a.BA.Version;
 
 @ShortName("bugly")
 @Author("icefairy333")
-@Version(1.00f)
-@DependsOn(values={"bugly_crash_release__2.1.1"})
-@Permissions(values={"android.permission.READ_PHONE_STATE" ,"android.permission.INTERNET" ,"android.permission.ACCESS_NETWORK_STATE" ,"android.permission.ACCESS_WIFI_STATE" ,"android.permission.READ_LOGS" })
+@Version(2.00f)
+@DependsOn(values={"bugly_crashreport_upgrade-1.2.1"})
+@Permissions(values={"android.permission.WRITE_EXTERNAL_STORAGE","android.permission.READ_PHONE_STATE" ,"android.permission.INTERNET" ,"android.permission.ACCESS_NETWORK_STATE" ,"android.permission.ACCESS_WIFI_STATE" ,"android.permission.READ_LOGS" })
 public class buglywrapper {
 	public void startCatching(String appid) {
-		if(appid.length()>0){
-			CrashReport.initCrashReport(BA.applicationContext,appid,false);
-		}else{
-			CrashReport.initCrashReport(BA.applicationContext);
-		}
+		Bugly.init(BA.applicationContext, appid, false);
+//		if(appid.length()>0){
+//			CrashReport.initCrashReport(BA.applicationContext,appid,false);
+//		}else{
+//			CrashReport.initCrashReport(BA.applicationContext);
+//		}
 	}
 	public void testJavaCrash() {
 		CrashReport.testJavaCrash();
+	}
+	public void checkUpgrade(boolean isManual,boolean isSilence ) {
+		Beta.checkUpgrade(isManual, isSilence );
 	}
 }
